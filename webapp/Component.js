b533bd6a95ebd889a95c3ae6d1443f7fd08bf382
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2024-11-22 22:50:57 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("firstelements.student10.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
